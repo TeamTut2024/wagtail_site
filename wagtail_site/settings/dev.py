fa48e87dceed8a86ed3019d9ac8a753ce1822e11
ideaktut@gmail.com <ideaktut@gmail.com>
2024-12-06 11:32:08 -0600

    Intialize wagtail site

---
+++ wagtail_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zo*^6hb1xnj@-pm%92+aq3)36*73@yn+(ba0yd$^fx5p-%s(lm"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
